8260cbfa9c970bba24a7633cfc4b202a0148a5c3
renaldi-oss <reynaldifp2000@gmail.com>
2022-02-28 13:00:00 +0700

    first commit

---
+++ public/assets/main.js
@@ -0,0 +1,49 @@
+let title = document.querySelector(".profile-name");
+const randomColor = () => {
+    let colors = ["#03a9fc", "#07fc03", "#de9d12", "#050505", "#b5f25a", "#bd5af2", "#f25aeb", "#5a9cf2", "#f25a9c", "#9e0402", "#1c029e", "#283d1a"];
+    let randomIndex = Math.floor(Math.random() * 13);
+    return colors[randomIndex]
+}
+title.addEventListener("click", () => {
+    let selectedColor = randomColor();
+    title.style.color = selectedColor;
+})
+
+var canvas = document.querySelector("canvas"),
+    ctx = canvas.getContext('2d');
+
+// Setting the width and height of the canvas
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Setting up the letters
+var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ';
+letters = letters.split('');
+
+// Setting up the columns
+var fontSize = 10,
+    columns = canvas.width / fontSize;
+
+// Setting up the drops
+var drops = [];
+for (var i = 0; i < columns; i++) {
+  drops[i] = 1;
+}
+
+// Setting up the draw function
+function draw() {
+  ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < drops.length; i++) {
+    var text = letters[Math.floor(Math.random() * letters.length)];
+    ctx.fillStyle = '#0f0';
+    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+    drops[i]++;
+    if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
+      drops[i] = 0;
+    }
+  }
+}
+
+// Loop the animation
+setInterval(draw, 33);
---
+++ resources/views/home.blade.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personal cv</title>
+    <link rel="stylesheet" href="assets/font/stylesheet.css">
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <canvas></canvas>
+        <div class="cv-page">
+            <div class="cv-header">
+                <div class="profile-widget">
+                    <div class="profile-info">
+                        <h1 class="profile-name">Reynaldi Fakhri Pratama</h1>
+                        <p class="user-title">Mahasiswa Politeknik Negeri Malang</p>
+                    </div>
+                    <div class="profile-image">
+                        <img src="assets/imgs/aldi.JPG">
+                    </div>
+                </div>
+            </div>
+            <div class="cv-body">
+                <div class="cv-skills-widget">
+                    <div class="skill-content-widget">
+                        <h2>Pendidikan</h2>
+                        <ul>
+                            <li>SDN Ketawanggedhe 2 Malang</li>
+                            <li>SMP Laboratorium UM Malang</li>
+                            <li>SMKN 5 Malang</li>
+                            <li>Politeknik Negeri Malang</li>
+                        </ul>
+                    </div>
+                    <div class="spacer"></div>
+                    <div class="skill-content-widget">
+                        <h2>Sertifikat</h2>
+                        <h3>CV agrindo</h3>
+                    </div>
+                    <div class="spacer"></div>
+                    <div class="skill-content-widget">
+                        <h2>Alamat</h2>
+                        <h3>JL.Semanggi Barat 21A</h3>
+                    </div>
+                    <div class="spacer"></div>
+                    <div class="skill-content-widget">
+                        <h2>NIM</h2>
+                        <h3>2041720209</h3>
+                    </div>
+                    <div class="spacer"></div>
+                    <div class="skill-content-widget">
+                        <h2>Agama</h2>
+                        <h3>islam</h3>
+                    </div>
+                    <div class="spacer"></div>
+                    <div class="skill-content-widget">
+                        <h2>TTL</h2>
+                        <h3>06-08-2000</h3>
+                    </div>
+                </div>
+                <div class="cv-info-widget">
+                    <div class="cv-info-content-widget">
+                        <h1>reynaldifp2000@gmail.com</h1>
+                        <p>081235267129</p>
+                        <p>Malang</p>
+                    </div>
+                    <div class="spacer"></div>
+                    <div class="cv-info-content-widget">
+                        <h1>Hobby</h1>
+                        <p>Gaming</p>
+                    </div>
+                    <div class="spacer"></div>
+                    <div class="cv-info-content-widget">
+                        <h1>Bahasa pemrograman yang dikuasai</h1>
+                        <p>Java</p>
+                        <p>C</p>
+                        <p>C++</p>
+                        <p>php</p>
+                    </div>
+                    <div class="spacer"></div>
+                    <div class="cv-info-content-widget">
+                        <h1>Bahasa</h1>
+                        <p>indonesia</p>
+                        <p>inggris</p>
+                    </div>
+                    <div class="spacer"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="assets/main.js"></script>
+</body>
+
+</html>
